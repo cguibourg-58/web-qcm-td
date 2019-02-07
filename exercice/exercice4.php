f47f5cf79811688618c7f76b5dd145289f2826dc
Clément Guibourg <cguibourg-pro@outlook.fr>
2019-02-07 09:18:20 +0100

    Exercice4

--- exercice/exercice4.php
+++ exercice/exercice4.php
@@ -4,6 +4,17 @@
  * the standard input according to the problem statement.
  **/
 $s = $args[0];
-
+$tabval = str_split($s);
+$i = 0;
+$result = 0;
 // Write an action using echo().
-echo('answer');
+while ($i != count($tabval)) {
+    if ($tabval[$i] == '0' || $tabval[$i] == '6' || $tabval[$i] == '9') {
+        $result+=1;
+    } else if ($tabval[$i] == '8') {
+        $result+=2;
+    }
+    $i++;
+}
+
+echo($result);
